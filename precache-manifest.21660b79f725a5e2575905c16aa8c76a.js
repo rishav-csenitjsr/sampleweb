068de9dc5f418c914b2d1a8374ab4d21c14b0541
Rishav Kumar <anshuthegreat97@gmail.com>
2019-07-11 00:24:31 +0530

    Updates

---
+++ precache-manifest.21660b79f725a5e2575905c16aa8c76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b182d20a86d4733c309c12b311e5deb5",
+    "url": "/sampleweb/index.html"
+  },
+  {
+    "revision": "5623e165d5edc3287a8f",
+    "url": "/sampleweb/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sampleweb/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5623e165d5edc3287a8f",
+    "url": "/sampleweb/static/js/main.f9515c94.chunk.js"
+  },
+  {
+    "revision": "c8b18d6b44c963413bc3",
+    "url": "/sampleweb/static/js/runtime~main.58940882.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sampleweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
